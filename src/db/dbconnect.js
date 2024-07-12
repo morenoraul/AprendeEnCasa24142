6a550bd55ee0c45e1cece7c6292623eb3b2bbcc2
morenoraul <moreno.r.alberto@gmail.com>
2024-07-11 23:40:33 -0300

    config

--- src/db/dbconnect.js
+++ src/db/dbconnect.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host :'mysql-aprenderencasa.alwaysdata.net',
   user :'368507_grupo19',
-  password:'@Prender3nc@sa2024',
+  password:'368507_grupo19',
   database:'aprenderencasa_db',
   port: 3306,
   waitForConnections: true,
