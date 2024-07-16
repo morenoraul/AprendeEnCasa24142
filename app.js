3b9fb567de09dbc6adcc1cdc1839b506dbf74cd4
morenoraul <moreno.r.alberto@gmail.com>
2024-07-15 22:27:26 -0300

    modificacion

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /* ***SERVIDOR ESTÁTICO CON EXPRESS (Módulo Externo)*** */
 const express = require(`express`)
 const override = require('method-override')
-const rutas = require('./src/routes/mainRoutes.js')
-const rutasLogin = require('./src/routes/LoginRoutes.js')
+const rutas = require('.mainRoutes.js')
+const rutasLogin = require('.loginRoutes.js')
 const app = express()
 
 const port = 8080 || process.env.PORT || 3000
@@ -25,21 +25,4 @@ app.use((req, res, next) =>{
 })
 
 app.listen(port, () => console.log(`Hola! Esto conetado en el puerto: ${port}`))
-
-/*  ***SERVER ESTÁTICO CON HTTP SERVER (Módulo Nativo)***
-const http = require(`http`)
-const fs = require(`fs`)
-
-const server = http.createServer((req, res) => {
-	const file = fs.readFileSync(__dirname + '/machete.html')
-	res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
-	res.end(file)
-})
-
-server.listen(8080, ()=> console.log(`Servidor arriba en el puerto 8080`)) */
-
-//const nombre = "Vlad"
-//console.log(`Hola ${nombre} estoy ejecutando un archivo desde Nodejs`)
-
-//res.end(`<h1 style="color: hotpink">Hola desde el server de prueba de Node!</h1>
-//	<p>Estoy enviando ahora un HTML</p>`)
+;
